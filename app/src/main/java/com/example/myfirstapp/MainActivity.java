553aa7c5cb7dd30bf6905145c624f00539499b00
Kulesh143 <kulesh143143@yahoo.com>
2021-11-03 12:15:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private TextView txt;
+private Button plus,minus,reset,btnshow;
+private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.txt1);
+        plus = findViewById(R.id.plus);
+        minus = findViewById(R.id.minus);
+        reset = findViewById(R.id.reset);
+        plus.setOnClickListener(this);
+        minus.setOnClickListener(this);
+        reset.setOnClickListener(this);
+        btnshow=findViewById(R.id.btnshow);
+        imageView=findViewById(R.id.imageView);
+        btnshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(imageView.getVisibility()==View.GONE){
+                    imageView.setVisibility(View.VISIBLE);
+                }else{
+                    imageView.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+//        plus.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                int counter= Integer.parseInt(txt.getText().toString());
+//                counter++;
+//                txt.setText(counter+"");
+//                Toast.makeText(MainActivity.this,counter+"",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        minus.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                int counter=Integer.parseInt(txt.getText().toString());
+//                if(counter!=0){
+//                counter--;
+//                txt.setText(counter+"");
+//                Toast.makeText(MainActivity.this,counter+"",Toast.LENGTH_SHORT).show();
+//
+//            }}
+//        });
+//        reset.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                txt.setText("0");
+//            }
+//        });
+//    }
+    }
+
+    public void plusm(View view) {
+        int counter= Integer.parseInt(txt.getText().toString());
+        counter++;
+        txt.setText(counter+"");
+        Toast.makeText(MainActivity.this,counter+"",Toast.LENGTH_SHORT).show();
+    }
+
+    public void minusm(View view) {
+        int counter=Integer.parseInt(txt.getText().toString());
+        if(counter!=0){
+            counter--;
+            txt.setText(counter+"");
+            Toast.makeText(MainActivity.this,counter+"",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public void resetm(View view) {
+        txt.setText("0");
+    }
+
+
+    public void operation(View view) {
+        int id=view.getId();
+        int counter=0;
+        switch(id){
+
+            case R.id.plus:
+                counter= Integer.parseInt(txt.getText().toString());
+                counter++;
+                txt.setText(counter+"");
+                break;
+               // Toast.makeText(MainActivity.this,counter+"",Toast.LENGTH_SHORT).show();
+            case R.id.minus:
+                counter= Integer.parseInt(txt.getText().toString());
+                if(counter!=0) {
+                    counter--;
+                    txt.setText(counter + "");
+                    //  Toast.makeText(MainActivity.this,counter+"",Toast.LENGTH_SHORT).show();
+                }break;
+                case R.id.reset:
+                txt.setText("0");
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id=view.getId();
+        int counter=0;
+        switch(id){
+
+            case R.id.plus:
+                counter= Integer.parseInt(txt.getText().toString());
+                counter++;
+                txt.setText(counter+"");
+                break;
+            // Toast.makeText(MainActivity.this,counter+"",Toast.LENGTH_SHORT).show();
+            case R.id.minus:
+                counter= Integer.parseInt(txt.getText().toString());
+                if(counter!=0) {
+                    counter--;
+                    txt.setText(counter + "");
+                    //  Toast.makeText(MainActivity.this,counter+"",Toast.LENGTH_SHORT).show();
+                }break;
+            case R.id.reset:
+                txt.setText("0");
+                break;
+        }
+    }
+}
